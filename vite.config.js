f9956f96b24550228cdc7e189d56781f12a85929
aslamlaiba713 <aslamlaiba713@gmail.com>
2026-05-19 12:24:24 +0500

    update changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Portfolio-Project/',
+  base: './Portfolio-Project/',
 })
